dd9a6c63691f39756851f307b8ae325ba309ed28
Stewart Park <stewartpark92@gmail.com>
2015-12-29 11:05:05 -0800

    Changed the duration to 7s.

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ while True:
     if run_train:
         print 'Chu chu~~~'
         control.go(control.FORWARD_SLOW)
-        time.sleep(15)
+        time.sleep(7)
         control.stop()
         print 'Stopped.'
     time.sleep(5)
