e97d0abea149b2ff9a9c4fcff4372ececd92a7bb
Stewart Park <stewartpark92@gmail.com>
2016-01-04 11:30:28 -0800

    Adding more poops on it.

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def git_diff_changes(repo, commit_1, commit_2):
     stats = short_stats.split(',')
     return int(stats[1].split()[0]) + int(stats[2].split()[0])
 
+
 def time_to_run(num_changes, min_run_time, max_run_time):
     """
     Description:
@@ -44,29 +45,31 @@ def time_to_run(num_changes, min_run_time, max_run_time):
     else:
         return time_to_run
 
-while True:
-    changes = 0
-    for i, repo in enumerate(repos):
-        path = repo['path']
-        print path
-        last_commit = repo['last_commit']
-        branch = repo['branch']
-        r = Repo(path)
-        try:
-            r.remotes.origin.pull()
-            lc = r.commit(branch).hexsha
-            if lc != last_commit:
-                changes += git_diff_changes(r, r.commit(lc), r.commit(last_commit))
-                repos[i]['last_commit'] = lc
-        except Exception, e:
-            print 'Oops.', e
 
-    if changes:
-        print 'Choo~~~'
-        control.choo()
-        control.go(control.FORWARD_SLOW)
-        time.sleep(time_to_run(changes, 5, 60))
-        print 'Stopped.'
-        control.stop()
-    time.sleep(5)
-    json.dump(repos, open("./repos.json", "w"))
+if __name__ == '__main__':
+    while True:
+        changes = 0
+        for i, repo in enumerate(repos):
+            path = repo['path']
+            print path
+            last_commit = repo['last_commit']
+            branch = repo['branch']
+            r = Repo(path)
+            try:
+                r.remotes.origin.pull()
+                lc = r.commit(branch).hexsha
+                if lc != last_commit:
+                    changes += git_diff_changes(r, r.commit(lc), r.commit(last_commit))
+                    repos[i]['last_commit'] = lc
+            except Exception, e:
+                print 'Oops.', e
+
+        if changes:
+            print 'Choo~~~'
+            control.choo()
+            control.go(control.FORWARD_SLOW)
+            time.sleep(time_to_run(changes, 5, 60))
+            print 'Stopped.'
+            control.stop()
+        time.sleep(5)
+        json.dump(repos, open("./repos.json", "w"))
