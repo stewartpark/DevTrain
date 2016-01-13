207f15fd1dc6c93aa0bbc645f8580e463e98ccf1
Stewart Park <stewartpark92@gmail.com>
2016-01-12 17:50:58 -0800

    Removed the stop controlling pin.

--- control.py
+++ control.py
@@ -25,7 +25,6 @@ import os
 
 # Hardware config
 PIN_SPEED_CTRL = 11
-PIN_STOP_CTRL = 13
 PWM = None
 
 FORWARD_SLOW =  50
@@ -41,13 +40,11 @@ def go(dutycycle):
     GPIO.setup(PIN_SPEED_CTRL, GPIO.OUT)
     PWM = GPIO.PWM(PIN_SPEED_CTRL, 30)
     PWM.start(dutycycle)
-    GPIO.setup(PIN_STOP_CTRL, GPIO.OUT)
 
 
 def stop():
     global PWM
     GPIO.setup(PIN_SPEED_CTRL, GPIO.IN)
-    GPIO.setup(PIN_STOP_CTRL, GPIO.IN)
     if PWM:
         PWM.stop()
         PWM = None
